67565c9bd42e559ad2f418cb692e78eca11bbf5f
ngtuanvuduy9 <ngtuanvuduy9@gmail.com>
2026-03-13 08:18:01 +0700

    demo

---
+++ nguyentuanvuduy_2123110226/Controllers/DemoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace nguyentuanvuduy_2123110226.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+    }
+}
